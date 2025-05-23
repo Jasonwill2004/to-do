2985bf3dc3d5fd753e40630d459e295c2d2a72fc
Your Name <your.email@example.com>
2025-05-23 22:01:59 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import path from "path";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
